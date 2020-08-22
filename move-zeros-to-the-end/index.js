bc9a84f71149def96aacafa5edf11eb45a6c33a2
Alejandro Garcia <agarc371@gmail.com>
2020-08-22 10:58:43 -0400

    add potential refactor (causing timeouts when tested)

--- move-zeros-to-the-end/index.js
+++ move-zeros-to-the-end/index.js
@@ -3,7 +3,6 @@ var moveZeros = function (arr) {
 
   //loop through array, remove any 0s from original array and push to zeros array.
   for (let i = 0; i < arr.length; i++) {
-    console.log(arr);
     if (arr[i] === 0) {
       arr.splice([i], 1);
       zeros.push(0);
@@ -14,6 +13,21 @@ var moveZeros = function (arr) {
   //loop through zeros array and push each added 0 to end of original array.
   for (let j = 0; j < zeros.length; j++) {
     arr.push(0);
+  }
+  return arr;
+};
+//   zeros.forEach(ele => arr.push(ele))
+
+//REFACTOR - Causes timeouts (is the loop infinite?)
+
+var moveZeros = function (arr) {
+  //loop through array, remove any 0s from original position of array and push to end of array.
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      arr.splice([i], 1);
+      arr.push(0);
+      i--; //reducing the index by 1 to account for shortening of array
+    }
   }
   return arr;
 };
